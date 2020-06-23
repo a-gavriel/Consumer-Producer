4ad6ee0306f3c98bb727f9ff40f369afe77aed21
xafilk <carva97valvi@gmail.com>
2020-06-22 20:01:12 -0600

    In Progress Consumer Process

--- src/consumer.c
+++ src/consumer.c
@@ -1,5 +1,5 @@
 #define _GNU_SOURCE
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h> 
 #include <string.h> 
 #include <fcntl.h> 
@@ -13,16 +13,31 @@
 #include <getopt.h>
 #include <sys/types.h>
 #include <stdbool.h>
+#include <semaphore.h>
+#include <fcntl.h>          /* O_CREAT, O_EXEC          */
 
 #include "../include/randomGenerators.h"
 
 #define MSGSIZE 16
 #define GLOB_SIZE 64
 
+bool flag = true;
 char *app_name = NULL;
 double middleSeconds = -1;
 char *buffer_name = NULL;
 bool isAutoMode = false;
+void *bufferPtr = NULL;
+uint32_t SIZE = 1; 
+
+//Begin Semaphore Region
+char *semBufferName = "sem_buffer";
+sem_t *semBuffer = NULL;
+// End Semaphore Region
+
+void InitializeSemaphores()
+{
+   semBuffer = sem_open(semBufferName, 0); /* Open a preexisting semaphore. */
+}
 
 /**
  * \brief Print help for this application
@@ -38,6 +53,107 @@ void print_help(void)
 	printf("\n");
 }
 
+/**
+ * Read Finalizar Global (Buffer) Variable
+*/
+bool ValidateFinalizerSignal()
+{
+    //If finalizar global variable is 1 return true
+}
+
+/**
+ * Exit Sequence (Finalizer Process start)
+ * **/
+void ExitFromFinalizerProcess()
+{
+
+}
+
+/**
+ * Exit Sequence (Read special message from buffer)
+ * **/
+void ExitFromSpecialMessage()
+{
+
+}
+
+/**
+ * Read Message from buffer
+*/
+char *ReadMessage()
+{
+
+}
+
+int SyncBuffer()
+{  
+    /* shared memory file descriptor */ /* create the shared memory object */
+    int shm_fd = shm_open(buffer_name, O_RDONLY, 0666); 
+  
+    /* pointer to shared memory obect */
+    bufferPtr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0); 
+    if (bufferPtr == MAP_FAILED){
+        perror("MMAP FAILED, Error mmapping the file, Buffer hasn't been created!\n");
+        return EXIT_FAILURE;
+    }
+    if(RemapBuffer() == 0)
+    {
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+}
+
+int RemapBuffer()
+{
+    uint8_t totmsgs;
+    memcpy(&totmsgs,bufferPtr,1); 
+    printf("Number of messages: %d\n", totmsgs);
+    uint32_t newSIZE = GLOB_SIZE + MSGSIZE*totmsgs; 
+
+    /* remap the shared memory object */
+    void* temp = mremap(bufferPtr, SIZE, newSIZE, MREMAP_MAYMOVE); 
+    if(temp == MAP_FAILED){
+        perror("REMAP FAILED, Error on mremap()");
+        return EXIT_FAILURE;
+    }
+    bufferPtr = temp;
+    return EXIT_SUCCESS;
+}
+
+
+void AutomatedConsumerProcess()
+{
+
+}
+
+void ManualConsumerProcess()
+{
+    short int exitMode = 0;
+    char *message = NULL;
+    while(flag)
+    {
+        if(ValidateFinalizerSignal())
+        {
+            exitMode = 0;
+            break;
+        }
+        if(getchar() == 10)
+        {
+            //Start Critical Region
+            sem_wait(semBuffer);           
+            message = ReadMessage();
+            sem_post(semBuffer);
+            //End Critical Region (Release Sem)
+        }
+    }
+    if(exitMode == 0)
+    {
+        ExitFromFinalizerProcess();
+    }
+}
 
 int main(int argc, char *argv[]){ 
 
@@ -87,49 +203,42 @@ int main(int argc, char *argv[]){
         isAutoMode = false;
     }
     
-
     printf("%s \n", buffer_name);
     printf("%lf \n", middleSeconds);
     printf("%d \n", isAutoMode);
 
-    /* the size (in bytes) of shared memory object */
-    uint32_t SIZE = 1; 
-  
-    /* shared memory file descriptor */ /* create the shared memory object */
-    int shm_fd = shm_open(buffer_name, O_RDONLY, 0666); 
-  
-    /* pointer to shared memory obect */
-    void* ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0); 
-    if (ptr == MAP_FAILED){
-        perror("MMAP FAILED, Error mmapping the file, Buffer hasn't been created!\n");
+    //Open Buffer
+    /*
+    if(SyncBuffer()  == 1)
+    {
         return EXIT_FAILURE;
     }
-
-    uint8_t totmsgs;
-    memcpy(&totmsgs,ptr,1); 
-    printf("Number of messages: %d\n", totmsgs);
-    uint32_t newSIZE = GLOB_SIZE + MSGSIZE*totmsgs; 
-
-    /* remap the shared memory object */
-    void* temp = mremap(ptr, SIZE, newSIZE, MREMAP_MAYMOVE); 
-    if(temp == MAP_FAILED){
-        perror("REMAP FAILED, Error on mremap()");
-        return EXIT_FAILURE;
+    */
+    //Select mode
+    if(isAutoMode)
+    {
+        AutomatedConsumerProcess();
     }
-    ptr = temp;
+    else
+    {
+        ManualConsumerProcess();
+    }
+    
 
+    
 
     // Sleep Section
 
 
 
-    //For testing!
-    uint8_t value1;
-    memcpy(&value1,ptr+50,1); 
-    printf("Value read: %d\n", value1);
 
-    memcpy(&value1,ptr+51,1); 
-    printf("Value read: %d\n", value1);
+    //For testing!
+    //uint8_t value1;
+    //memcpy(&value1,bufferPtr+50,1); 
+    //printf("Value read: %d\n", value1);
+
+    //memcpy(&value1,bufferPtr+51,1); 
+    //printf("Value read: %d\n", value1);
 
     return 0; 
 } 
